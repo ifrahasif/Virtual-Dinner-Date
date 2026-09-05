f4c5424884414fc3e2a770937a5755bc2ae5fbe9
ifrahasif <ifrahasif30@gmail.com>
2026-09-05 14:00:54 +0200

    Updates

---
+++ precache-manifest.22d859b4ad5a92d7b8ed3e775ec06a55.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dda8c23c6138855b7a618d6ce7e980",
+    "url": "/Virtual-Dinner-Date/index.html"
+  },
+  {
+    "revision": "559eb03f8e5cb4c86956",
+    "url": "/Virtual-Dinner-Date/static/css/main.a26ea75f.chunk.css"
+  },
+  {
+    "revision": "ba1aaf3230d4f39b822f",
+    "url": "/Virtual-Dinner-Date/static/js/2.2482cadb.chunk.js"
+  },
+  {
+    "revision": "559eb03f8e5cb4c86956",
+    "url": "/Virtual-Dinner-Date/static/js/main.192a2567.chunk.js"
+  },
+  {
+    "revision": "8d29678039a4f285f5f5",
+    "url": "/Virtual-Dinner-Date/static/js/runtime~main.5fe4626d.js"
+  },
+  {
+    "revision": "d4e68cce4ba0a4ccf4daf2df11482b6a",
+    "url": "/Virtual-Dinner-Date/static/media/angrycat.d4e68cce.gif"
+  },
+  {
+    "revision": "a3d69709008b07c99616d5c78df28bd7",
+    "url": "/Virtual-Dinner-Date/static/media/catplay.a3d69709.gif"
+  },
+  {
+    "revision": "169b1dc07598d7de23fc016c25b05991",
+    "url": "/Virtual-Dinner-Date/static/media/chickenburger.169b1dc0.jpg"
+  },
+  {
+    "revision": "66e9fdeca1137fe0d05f3e901e580f2f",
+    "url": "/Virtual-Dinner-Date/static/media/chickenwings.66e9fdec.jpg"
+  },
+  {
+    "revision": "c427093825d36c6e76ca91c8c80569a7",
+    "url": "/Virtual-Dinner-Date/static/media/cutecat.c4270938.gif"
+  },
+  {
+    "revision": "bf8d2005b7e9b04a3e0176c76b101fb7",
+    "url": "/Virtual-Dinner-Date/static/media/pizza.bf8d2005.jpg"
+  },
+  {
+    "revision": "33892d44ce52ea7d0a5319a304c2c207",
+    "url": "/Virtual-Dinner-Date/static/media/shawarma.33892d44.jpeg"
+  },
+  {
+    "revision": "8b1b7cf2872e0bb7ee272eca4c94dbc8",
+    "url": "/Virtual-Dinner-Date/static/media/tabouleh.8b1b7cf2.jpg"
+  },
+  {
+    "revision": "6315c254150d512c6061fabbfc626463",
+    "url": "/Virtual-Dinner-Date/static/media/tacos.6315c254.jpg"
+  }
+]);
